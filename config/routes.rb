7c70513bf72e90967c5e5792e46303f71bca8ca1
mayur_kumbhar <mayuresh919@gmail.com>
2016-04-15 10:52:32 +0000

    messeges successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
   
+  get 'welcome/index'
+
   devise_for :users
-  root to: 'pages#index'
-  get 'pages/about'
+
+Rails.application.routes.draw do
+  root 'welcome#index'
+end
+
+get 'pages/about'
   get 'pages/contact'
   
+  get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+  get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+  get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
+  
+  resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+    end
+  end
 end
